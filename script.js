26ccfdf59aaf28c1cd7781bcd57b72ba58160b62
Samuel Santos <samuka0102128@gmail.com>
2023-02-14 17:16:10 -0300

    Adicionando responsividade na navbar, com opcao de remover e adiciona-la clicando nom menu hamburguer

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const $favorite = document.querySelector('.favorite')
 const $hearth = document.querySelector('#hearth')
+const $menu = document.querySelector('#menu')
+const $nav = document.querySelector('nav')
+
 
 $favorite.addEventListener('click', ()=>{
     $hearth.classList.toggle('active')
 })
+
+$menu.addEventListener('click',()=>{
+    $nav.classList.toggle('active-menu')
+
+    $menu.classList.toggle('fa-bars')
+    $menu.classList.toggle('fa-xmark')
+})
+
